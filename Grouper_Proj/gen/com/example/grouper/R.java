ed7b796d6b98fe861f80161ef5a83cb0d92d05e5
wertzy1334 <wertzy_1334@yahoo.com>
2014-11-17 13:54:04 -0500

    Revert "Searchable Activity"
    This reverts commit a313be4888fc1dbfc9aa5f6434abd2376d6e1fed.

--- Grouper_Proj/gen/com/example/grouper/R.java
+++ Grouper_Proj/gen/com/example/grouper/R.java
@@ -10,53 +10,36 @@ package com.example.grouper;
 public final class R {
     public static final class attr {
     }
-    public static final class dimen {
-        /**  Default screen margins, per the Android Design guidelines. 
-
-         Example customization of dimensions originally defined in res/values/dimens.xml
-         (such as screen margins) for screens with more than 820dp of available width. This
-         would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
-    
-         */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
-    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int ic_menu_search=0x7f020001;
         public static final int ic_sysbar_quicksettings=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int fragment_container=0x7f080000;
-        public static final int menuitem_search=0x7f080001;
-        public static final int menuitem_settings=0x7f080002;
+        public static final int fragment_container=0x7f070000;
+        public static final int menuitem_search=0x7f070001;
+        public static final int menuitem_settings=0x7f070002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int activity_searchable=0x7f030001;
-        public static final int discoverfragment=0x7f030002;
-        public static final int feedfragment=0x7f030003;
-        public static final int todofragment=0x7f030004;
+        public static final int discoverfragment=0x7f030001;
+        public static final int feedfragment=0x7f030002;
+        public static final int todofragment=0x7f030003;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
-        public static final int searchable=0x7f070001;
+        public static final int main=0x7f060000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050008;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050007;
-        public static final int title_activity_searchable=0x7f050006;
-        /**  Menu Strings 
+        public static final int app_name=0x7f040000;
+        /** Menu Strings 
          */
-        public static final int ui_menu_search=0x7f050001;
-        public static final int ui_menu_settings=0x7f050002;
-        public static final int ui_tabname_discover=0x7f050004;
-        /**  Tab Strings 
+        public static final int ui_menu_search=0x7f040001;
+        public static final int ui_menu_settings=0x7f040002;
+        public static final int ui_tabname_discover=0x7f040004;
+        /** Tab Strings
          */
-        public static final int ui_tabname_feed=0x7f050003;
-        public static final int ui_tabname_todo=0x7f050005;
+        public static final int ui_tabname_feed=0x7f040003;
+        public static final int ui_tabname_todo=0x7f040005;
     }
     public static final class style {
         /** 
@@ -80,11 +63,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f050000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060002;
-        public static final int MyActionBarTabTextStyle=0x7f060001;
+        public static final int AppTheme=0x7f050002;
+        public static final int MyActionBarTabTextStyle=0x7f050001;
     }
 }
