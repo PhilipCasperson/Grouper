333f30f8457e8a931363e41b476fc444355c7ff8
Philip Casperson <philip.casperson@gmail.com>
2014-11-23 21:50:37 -0500

    -added and commented out a method in SearchableActivity

--- Grouper_Proj/src/com/example/grouper/SearchableActivity.java
+++ Grouper_Proj/src/com/example/grouper/SearchableActivity.java
@@ -2,9 +2,14 @@ package com.example.grouper;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+
+import com.example.grouper.database.DatabaseHandler;
+import com.example.grouper.database.Group;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -13,6 +18,7 @@ import android.text.Html;
 import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -23,11 +29,13 @@ public class SearchableActivity extends Activity {
 	//ListView
 	private ListView lv;
 	//ListView Adapter
-	ArrayAdapter<String> adapter;
+	ArrayAdapter<Group> adapter;
 	//Search EditText
 	EditText inputSearch;
 	//ArrayList for ListView
 	ArrayList<HashMap<String, String>> groupList;
+	DatabaseHandler db;
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -38,15 +46,18 @@ public class SearchableActivity extends Activity {
 		actionbar.setBackgroundDrawable(new ColorDrawable(Color.WHITE));
 		actionbar.setTitle(Html.fromHtml("<font color='#000000'>Search Groups</font>"));
 		
+		List<Group> groupList = db.getAllgroups();
+		
+		
 		//Listveiw Data
-		String groups[] = {"Group 1", "Mobile App Dev", "Database Mgmt", "Database mgm 2",
-							"iPhone dev", "Java", "C#", "Networking", "Group 3", "LAN Group"};
+		//String groups[] = {"Group 1", "Mobile App Dev", "Database Mgmt", "Database mgm 2",
+							//"iPhone dev", "Java", "C#", "Networking", "Group 3", "LAN Group"};
 		
 		lv = (ListView) findViewById(R.id.list_view);
 		inputSearch = (EditText) findViewById(R.id.inputSearch);
 		
 		//Adding items to ListView
-		adapter = new ArrayAdapter<String>(this, R.layout.searchable_list_item, R.id.searchable_group_name, groups);
+		adapter = new ArrayAdapter<Group>(this, R.layout.searchable_list_item, R.id.searchable_group_name, groupList);
 		lv.setAdapter(adapter);
 		
 		/**
@@ -94,4 +105,11 @@ public class SearchableActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+//	public void onListItemClick(ListView l, View v, int position, long id){
+//		Group group = (Group)getListAdapter().getItem(position);
+//		Intent intent = new Intent(getActivity().getApplicationContext(), GroupViewActivity.class);
+//		intent.putExtra("index", group.getID());
+//		startActivity(intent);
+//	}
 }
