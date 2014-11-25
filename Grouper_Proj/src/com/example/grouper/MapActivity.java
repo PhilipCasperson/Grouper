9d31e40fad370d7a1e2b62d3146f41aae06e2579
wertzy1334 <wertzy_1334@yahoo.com>
2014-11-25 12:44:03 -0500

    More Maps
    Map files updated with some logic. Should be close to working but
    emulator doesn't support the google play services. This can only be
    tested on an android device. Followed a pretty good tutorial here:
    http://www.androidhive.info/2013/08/android-working-with-google-maps-v2/

--- Grouper_Proj/src/com/example/grouper/MapActivity.java
+++ Grouper_Proj/src/com/example/grouper/MapActivity.java
@@ -1,20 +1,54 @@
 package com.example.grouper;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.location.Criteria;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MapActivity extends Activity {
 
+	//Google Map
+	private GoogleMap googleMap;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_map);
+		
+		try {
+			//loading map
+			InitializeMap();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
+	private void InitializeMap() {
+		if (googleMap == null) {
+			googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+			
+			//Check if map is created successfully or not
+			if (googleMap == null) {
+				Toast.makeText(getApplicationContext(), "Sorry! unable to create maps", Toast.LENGTH_SHORT).show();
+			}
+		}
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		InitializeMap();
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater inflater = getMenuInflater();
