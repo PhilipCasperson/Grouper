7cbd5c372ac192810a70800a815c8a791d18432d
Jared Handra <jared.handra@gmail.com>
2014-11-30 21:55:35 -0500

    Got the map working

--- Grouper_Proj/src/com/example/grouper/MapActivity.java
+++ Grouper_Proj/src/com/example/grouper/MapActivity.java
@@ -1,7 +1,10 @@
 package com.example.grouper;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.app.Activity;
 import android.content.Context;
@@ -26,16 +29,45 @@ public class MapActivity extends Activity {
 		
 		try {
 			//loading map
-			InitializeMap();
+			initializeMap();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	private void InitializeMap() {
+	private void initializeMap() {
 		if (googleMap == null) {
 			googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+			//set map to UC
+			LatLng uniCin = new LatLng(39.133122, -84.514950);
 			
+			//dummy data
+			LatLng meeting1 = new LatLng(39.132925, -84.516475);
+			LatLng meeting2 = new LatLng(39.131989, -84.515123);
+			LatLng meeting3 = new LatLng(39.131390, -84.511808);
+			LatLng meeting4 = new LatLng(39.127531, -84.516568);
+			LatLng meeting5 = new LatLng(39.128136, -84.517617);
+			LatLng meeting6 = new LatLng(39.127832, -84.514144);
+			
+			googleMap.setMyLocationEnabled(true);
+			googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(uniCin, 15));
+			
+			googleMap.addMarker(new MarkerOptions()
+				.title("Discuss Midterm")
+				.snippet("8:30, Database Group")
+				.position(meeting1));
+			googleMap.addMarker(new MarkerOptions()
+			.title("Work on Final")
+			.snippet("6:00, Android Dev Group")
+			.position(meeting2));
+			googleMap.addMarker(new MarkerOptions()
+			.title("DX Chapter Meeting")
+			.snippet("8:30")
+			.position(meeting3));
+			googleMap.addMarker(new MarkerOptions()
+			.title("Discuss Upcoming Goals")
+			.snippet("4:00, iPhone Dev Group")
+			.position(meeting4));
 			//Check if map is created successfully or not
 			if (googleMap == null) {
 				Toast.makeText(getApplicationContext(), "Sorry! unable to create maps", Toast.LENGTH_SHORT).show();
@@ -46,7 +78,7 @@ public class MapActivity extends Activity {
 	@Override
 	protected void onResume() {
 		super.onResume();
-		InitializeMap();
+		initializeMap();
 	}
 	
 	@Override
